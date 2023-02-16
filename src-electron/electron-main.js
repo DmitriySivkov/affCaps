ba32715d43b9ba0f9a326a4c9024c10a64f96db8
Dmitrii Sivkov <sivkovdw@yandex.ru>
2023-02-16 17:40:47 +0300

    electron default resolution

--- src-electron/electron-main.js
+++ src-electron/electron-main.js
@@ -22,8 +22,8 @@ function createWindow () {
    */
 	mainWindow = new BrowserWindow({
 		icon: path.resolve(__dirname, "icons/icon.png"), // tray icon
-		width: 1000,
-		height: 600,
+		width: 1280,
+		height: 640,
 		frame: false,
 		useContentSize: true,
 		webPreferences: {
